4eefa5d1b5439694d2fab1150e1646e1775cff04
megamegax <megamegax@gmail.com>
2025-05-16 16:07:25 +0200

    feat(sh): add shellenv when creating folders

--- setup/create-default-folders.sh
+++ setup/create-default-folders.sh
@@ -1,6 +1,13 @@
 export WORK=$HOME/development/work
 export HOBBY=$HOME/development/hobby
 
+  (echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> $HOME/.zprofile
+        eval "$(/usr/local/bin/brew shellenv)"
+        # For testing in container
+        echo >> /root/.bashrc
+        echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /root/.bashrc
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 mkdir $HOME/development
 mkdir $HOME/development/work
 mkdir $HOME/development/hobby
